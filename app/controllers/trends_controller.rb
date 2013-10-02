93585e86275a9a50c82ed297eba541df78e33967
michael TanLiao <michael.tanliao@gmail.com>
2013-10-02 18:29:29 -0500

    oauth

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -5,7 +5,7 @@ class TrendsController < ApplicationController
     	require "open-uri"
     	require "youtube_it"
     	require "rubygems"
-    	require "oAuth"
+    	require "oauth"
     	
 	class Panel
 		attr_accessor :videos, :id, :boardId, :title, :description, :url, :thumbnailUrl, :popularity
